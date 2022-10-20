2a6222c914e4210676ece6a2da8136dbce2a5d7e
Sadcat69 <arsenkabuska@gmail.com>
2022-10-20 17:51:27 +0300

    commit

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-a = 5
-print(a)
+# a = 5
+# print(a)
+#
+# class Student:
+#     def __init__(self):
+#         self.height = 165
+#         print("Im alive")
+#
+# nick = Student()
+# print(nick.height)
+
+# class Student:
+#     def __init__(self, name, height, age):
+#         self.name = name
+#         self.height = height
+#         self.age = age
+#
+# nick = Student()
+# print(nick.age)
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+print(self.side)
